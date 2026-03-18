67adda3ae78e9f61dab18a50e8765ab16c856a1e
Sevak <sevakmikichyan777@gmail.com>
2026-03-19 00:31:28 +0400

    remove some videos, reduce words and add one video

--- app/works/bread/page.tsx
+++ app/works/bread/page.tsx
@@ -1,86 +1,3 @@
-// "use client"
-
-// import { Container, Main, Page } from "@/common/components/layout"
-// import Header from "@/common/components/layout/header/header.component"
-// import { Title } from "@/common/components/typography";
-// import { Carousel } from "@/common/components/ui"
-// import { useEffect } from "react";
-
-// export default function Bread() {
-
-//   const slides = [
-//     "https://www.youtube.com/embed/k8R25T6LAMQ?si=LrUhs9InGtCFIvpS",
-//     "https://www.youtube.com/embed/dF2XJD0dJSk?si=00CxDQOvN325FLXL",
-//     "https://www.youtube.com/embed/XDyUVrgiqis?si=1gpSZPPLGsE0_8nQ",
-//     "/images/bread/wheat-field.jpg",
-//     "https://www.youtube.com/embed/62txhuiOvgM?si=WIHWzxBBcVyDdEcL",
-//     "https://www.youtube.com/embed/1wErAeUsUVU?si=NxbvNxTlMntwVmPe&start=425",
-//     "/images/bread/forest.jpg",
-//   ];
-
-//   useEffect(() => {
-//     const originalBackground = document.body.style.background;
-//     document.body.style.backgroundImage = "url('/images/bread/main.jpg')";
-//     document.body.style.backgroundSize = "cover";
-//     document.body.style.backgroundRepeat = "no-repeat";
-//     document.body.style.backgroundPosition = "center";
-
-//     return () => {
-//       document.body.style.background = originalBackground;
-//     };
-//   }, []);
-
-//   return (
-//     <Page name="bread">
-//       <Header />
-//       <Main>
-//         <Container>
-//           <Title heading={1} align="center" className="font-adamathuz">Հաց</Title>
-//           <Carousel className="w-full h-125 md:h-150" breakpoints={{
-//             480: {
-//               slidesPerView: 1,
-//               spaceBetween: 0,
-//             },
-//             768: {
-//               slidesPerView: 1,
-//               spaceBetween: 0,
-//             },
-//             1024: {
-//               slidesPerView: 1,
-//               spaceBetween: 0,
-//             },
-//           }}>
-//             {slides.map((slide, index) => {
-//               return (
-//                 <Carousel.Slide key={index} className="h-full w-full">
-//                   <div className="w-full h-full rounded-md overflow-hidden">
-//                     {slide.startsWith("/images") && (
-//                       <img
-//                         src={slide}
-//                         alt="Wheat Field"
-//                         className="object-contain object-center w-full h-full"
-//                       />
-//                     )}  {slide.startsWith("https") && (
-//                       <iframe
-//                         className="w-full h-full"
-//                         src={slide}
-//                         title="YouTube video player"
-//                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-//                         allowFullScreen
-//                       />
-//                     )}
-//                   </div>
-//                 </Carousel.Slide>
-//               )
-//             })}
-//           </Carousel>
-//         </Container>
-//       </Main>
-//     </Page>
-//   )
-// }
-
-
 "use client"
 
 import { Container, Main, Page } from "@/common/components/layout"
@@ -176,22 +93,6 @@ export default function Bread() {
 1. Կրակը թռավ քարից, Երի, երի, էվա, օ՜յ, օ՜յ, Ես կմեռնեմ քո դարդից, էվա, յարո ջան
 2. Ծով ա դառել մեր դաշտը, Երի, երի, էվա, օ՜յ, օ՜յ, Լիքը հաց ա մեր տաշտը, էվա յարո ջան
 3. Հաց կթխեմ, գարի ա, Երի, երի, էվա, օ՜յ, օ՜յ, Էս ինչ դժվար տարի ա, Շեկո ջան, էվա, օ՜յ, օ՜յ
-
-1. Կրակը թռավ քարից, Երի, երի, էվա, օ՜յ, օ՜յ, Ես կմեռնեմ քո դարդից, էվա, յարո ջան
-2. Ծով ա դառել մեր դաշտը, Երի, երի, էվա, օ՜յ, օ՜յ, Լիքը հաց ա մեր տաշտը, էվա յարո ջան
-3. Հաց կթխեմ, գարի ա, Երի, երի, էվա, օ՜յ, օ՜յ, Էս ինչ դժվար տարի ա, Շեկո ջան, էվա, օ՜յ, օ՜յ
-
-1. Կրակը թռավ քարից, Երի, երի, էվա, օ՜յ, օ՜յ, Ես կմեռնեմ քո դարդից, էվա, յարո ջան
-2. Ծով ա դառել մեր դաշտը, Երի, երի, էվա, օ՜յ, օ՜յ, Լիքը հաց ա մեր տաշտը, էվա յարո ջան
-3. Հաց կթխեմ, գարի ա, Երի, երի, էվա, օ՜յ, օ՜յ, Էս ինչ դժվար տարի ա, Շեկո ջան, էվա, օ՜յ, օ՜յ
-
-1. Կրակը թռավ քարից, Երի, երի, էվա, օ՜յ, օ՜յ, Ես կմեռնեմ քո դարդից, էվա, յարո ջան
-2. Ծով ա դառել մեր դաշտը, Երի, երի, էվա, օ՜յ, օ՜յ, Լիքը հաց ա մեր տաշտը, էվա յարո ջան
-3. Հաց կթխեմ, գարի ա, Երի, երի, էվա, օ՜յ, օ՜յ, Էս ինչ դժվար տարի ա, Շեկո ջան, էվա, օ՜յ, օ՜յ
-
-1. Կրակը թռավ քարից, Երի, երի, էվա, օ՜յ, օ՜յ, Ես կմեռնեմ քո դարդից, էվա, յարո ջան
-2. Ծով ա դառել մեր դաշտը, Երի, երի, էվա, օ՜յ, օ՜յ, Լիքը հաց ա մեր տաշտը, էվա յարո ջան
-3. Հաց կթխեմ, գարի ա, Երի, երի, էվա, օ՜յ, օ՜յ, Էս ինչ դժվար տարի ա, Շեկո ջան, էվա, օ՜յ, օ՜յ
 `
     },
     { type: "image", src: "/images/bread/wheat-field.jpg" },
@@ -209,21 +110,6 @@ export default function Bread() {
 Խաղ կանչող իմ ախպերը ցորեն եմ ցանե, կպտտի արտերը ցորեն եմ ցանե։
 Ախպեր ջան բաղովն արի, ցորեն եմ ցանե, ոտներդ շաղով արի ցորեն եմ ցանե
 Ցորեն եմ ցանել արտը ծլե կանաչել, հեռու տեղ ախպերմ ունիմ,ականջը կանչե։
-
-Ցորեն եմ ցանել արտը ծլե կանաչել, հեռու տեղ ախպերմ ունիմ,ականջը կանչե։
-Կաքավ եմ քարի միջին, ցորեն եմ ցանե, ձեն կուտամ ձորի միջին, ցորեն եմ ցանե։
-Հանդեն ծաղիկ քաղել եմ, ցորեն եմ ցանե, դաստա արել, կապել եմ, ցորեն եմ ցանե։
-Ցորեն եմ ցանել արտը ծլե կանաչել, հեռու տեղ ախպերմ ունիմ,ականջը կանչե։
-Կաքավ եմ սար ման կուգամ, ցորեն եմ ցանե, բլբուլ եմ բաղ ման կուգամ, ցորեն եմ ցանե։
-Գնացել ու եկել եմ ցորեն եմ ցանե, չարոխ գուլպես կոկել եմ ցորեն եմ ցանել։
-Ցորեն եմ ցանել արտը ծլե կանաչել, հեռու տեղ ախպերմ ունիմ,ականջը կանչե։
-Խաղ կանչող իմ ախպերը ցորեն եմ ցանե, կպտտի արտերը ցորեն եմ ցանե։
-Ախպեր ջան բաղովն արի, ցորեն եմ ցանե, ոտներդ շաղով արի ցորեն եմ ցանե
-Ցորեն եմ ցանել արտը ծլե կանաչել, հեռու տեղ ախպերմ ունիմ,ականջը կանչե։
-
-Գնանք տատիկին բերենք լավաշի ալյուր մաղենք
-Տատիկս ալյուր կմաղե, վըր վթներին կշաղեր, վեշթո նրան կկողեր։
-Տատիկ զալուր կըմաղեր, վըր փեշերուն կըշաղեր, վեշթո նրան կկողեր։
 `
     },
     { type: "video", src: "/videos/bread/mult.mp4" },
@@ -252,42 +138,36 @@ export default function Bread() {
 Տատիկս ալյուր կմաղե, Տատիկս ալյուր կմաղե, Տատիկս ալյուր կմաղե վըր վթներին կշաղեր։ Վըր վթներին կըշաղեր, վեշթո նրան կկողեր։
 
 Տատիկ զալուր կըմաղեր, Տատիկ զալուր կըմաղեր, Տատիկ զալուր կըմաղեր Վըր փեշերուն կըշաղեր: Վըր փեշերուն կըշաղեր, վեշթո նրան կկողեր։
-
-Գնանք տատիկին բերենք լավաշի ալյուր մաղենք Գնանք տատիկին բերենք լավաշի ալյուր մաղենք
-Տատիկս ալյուր կմաղե, Տատիկս ալյուր կմաղե, Տատիկս ալյուր կմաղե վըր վթներին կշաղեր։ Վըր վթներին կըշաղեր, վեշթո նրան կկողեր։
-
-Տատիկ զալուր կըմաղեր, Տատիկ զալուր կըմաղեր, Տատիկ զալուր կըմաղեր Վըր փեշերուն կըշաղեր: Վըր փեշերուն կըշաղեր, վեշթո նրան կկողեր։
-
-Գնանք տատիկին բերենք լավաշի ալյուր մաղենք Գնանք տատիկին բերենք լավաշի ալյուր մաղենք
-Տատիկս ալյուր կմաղե, Տատիկս ալյուր կմաղե, Տատիկս ալյուր կմաղե վըր վթներին կշաղեր։ Վըր վթներին կըշաղեր, վեշթո նրան կկողեր։
-
-Տատիկ զալուր կըմաղեր, Տատիկ զալուր կըմաղեր, Տատիկ զալուր կըմաղեր Վըր փեշերուն կըշաղեր: Վըր փեշերուն կըշաղեր, վեշթո նրան կկողեր։
 `
     },
     { type: "image", src: "/images/bread/bread.jpg" },
-    { type: "iframe", src: "https://www.youtube.com/embed/jauXEC9TLGg?si=PRlXkROzEFD0MWiC" },
-    { type: "video", src: "/videos/bread/krunk.mp4" },
     {
       type: "video",
-      src: "/videos/bread/artsakh.mp4"
+      src: "/videos/bread/erger.mov"
     },
-    {
-      type: "video",
-      objectFit: "contain",
-      src: "/videos/bread/sareri-hovin-mernem.mp4"
-    },
-    {
-      type: "video",
-      src: "/videos/bread/duduk.mp4"
-    },
-    {
-      type: "iframe",
-      src: "https://www.youtube.com/embed/LvMhfFwP_eI?si=ThtvXFVr68rNPyig"
-    },
-    {
-      type: "video",
-      src: "/videos/bread/erebuni.mp4"
-    },
+    // { type: "iframe", src: "https://www.youtube.com/embed/jauXEC9TLGg?si=PRlXkROzEFD0MWiC" },
+    // { type: "video", src: "/videos/bread/krunk.mp4" },
+    // {
+    //   type: "video",
+    //   src: "/videos/bread/artsakh.mp4"
+    // },
+    // {
+    //   type: "video",
+    //   objectFit: "contain",
+    //   src: "/videos/bread/sareri-hovin-mernem.mp4"
+    // },
+    // {
+    //   type: "video",
+    //   src: "/videos/bread/duduk.mp4"
+    // },
+    // {
+    //   type: "iframe",
+    //   src: "https://www.youtube.com/embed/LvMhfFwP_eI?si=ThtvXFVr68rNPyig"
+    // },
+    // {
+    //   type: "video",
+    //   src: "/videos/bread/erebuni.mp4"
+    // },
     {
       type: "images",
       className: "object-contain",
@@ -444,7 +324,7 @@ export default function Bread() {
                   )}
 
                   {hasWords(slide) && (
-                    <div className="w-[30%] h-full overflow-y-auto bg-black/50 text-white p-4 whitespace-pre-wrap">
+                    <div className="w-[30%] h-full overflow-y-auto bg-black/50 text-white p-4 whitespace-pre-wrap text-lg">
                       {slide.words}
                     </div>
                   )}
